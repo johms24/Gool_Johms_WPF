ffec4873e00fe09778fe55bee64d51d49cbd1942
johms24 <rjg@iname.com>
2014-12-05 02:34:06 -0500

    Cannot get the alerts to show up in browser
    Checking for a solution.  Maybe it works on the Mac but not the PC for
    some reason, or maybe I need to upgrade my Java.

--- GoToTrainingWeek2/js/script.js
+++ GoToTrainingWeek2/js/script.js
@@ -1,6 +1,17 @@
-/**
- * Created by rjgi on 12/5/2014.
+/*
+Johms Gool
+SDI Section 3
+Go To Training Week #2
+12-5-2014
  */
 
+//alert("Testing if the js file is connected.");
 
-alert("Testing if the js file is connected.");
+// Create an age calculator
+
+//Ask the user their name
+var name = prompt("Please type in your name:");
+
+//Use the variable and say hello to the user
+alert("Welcome "+name+"! Let's figure out how old you are");
+
