dade65e10d2a9e379abda4cf376012f02890498e
johms24 <rjg@iname.com>
2014-12-16 21:42:25 -0500

    Set up fruit array
    Set up fruit array

---
+++ GoToTrainingWeek3/js/week3.js
@@ -0,0 +1,22 @@
+/*
+Celina Gool
+SDI Section #3
+Go To Training week #3
+12-10-2014
+*/
+
+//alert("Testing To Make Sure we are working.");
+
+//Create code that sorts through fruit names and picks out pears
+
+//Create our "fruit bowl" arra
+
+var bowlOfFruit = ["apple","banana","pear","peach"];
+
+//Console logs the WHOLE array
+console.log(bowlOfFruit);
+
+
+//How can I get one fruit inside of the bowl?
+console.log(bowlOfFruit[1]);
+
