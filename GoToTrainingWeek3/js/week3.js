e6ba4869640d0ad01c9b9caccd2307b063e40813
johms24 <rjg@iname.com>
2014-12-16 23:33:39 -0500

    Validated with if statements
    Validated with if statements

--- GoToTrainingWeek3/js/week3.js
+++ GoToTrainingWeek3/js/week3.js
@@ -1,5 +1,5 @@
 /*
-Celina Gool
+Dan Williams
 SDI Section #3
 Go To Training week #3
 12-10-2014
@@ -9,7 +9,7 @@ Go To Training week #3
 
 //Create code that sorts through fruit names and picks out pears
 
-//Create our "fruit bowl" arra
+//Create our "fruit bowl" array
 
 var bowlOfFruit = ["apple","apple","banana","pear","peach","pear","tomato","kiwi","pear","banana","pear"];
 
@@ -105,3 +105,38 @@ for(var i=0; i<bowlOfFruit.length ; i++){
     }
 }
 console.log("Total number of pears in the bowl is "+pearNumber);
+
+
+
+//Using conditionals to validate prompts
+
+//ask the user for input
+var userInput = prompt("Please type in your name.");
+
+if(userInput===""){
+    //The user left it blank
+
+    //Re-prompt the user
+    userInput=prompt("Please do not leave blank.  What is your name?");
+
+}//how do I test if the user left it blank
+
+console.log("Welcome "+userInput);
+
+
+//Test if the user types in a number?
+//isNaN() - Is Not A Number
+
+console.log(isNaN("test"));
+console.log(isNaN(7));
+
+var userNumber = prompt("Type in any number");
+//Test if it is a number
+if(isNaN(userNumber)){
+
+    //This is a text string
+    //reprompt the user for a number
+    userNumber= prompt("Only type in numbers!");
+    
+
+}
