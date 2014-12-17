4a4b3627e4414a707b70613a407407e2666b8831
johms24 <rjg@iname.com>
2014-12-16 21:58:42 -0500

    Tested first fruit in the bowl array
    Tested first item in array

--- GoToTrainingWeek3/js/week3.js
+++ GoToTrainingWeek3/js/week3.js
@@ -20,3 +20,29 @@ console.log(bowlOfFruit);
 //How can I get one fruit inside of the bowl?
 console.log(bowlOfFruit[1]);
 
+//How many fruits are in the bowl?
+//How many items are in an array?
+//length of the array
+//Dot syntax - "use a period!"
+console.log(bowlOfFruit.length);
+
+//What is the last index number of the array, using the length property?
+// Last index number is ALWAYS one less than the length.
+// bowlOfFruit.length - 1 = last index #
+
+//Create a variable to track the number of pears we have
+var totalNumPears = 0;
+
+//Test each item and see if it is a pear
+
+//if(condition to test) { Code to run if true" }
+
+if(bowlOfFruit[0]==="pear"){
+     //this will run if true
+     //add one to our counting variable
+    console.log("This fruit is a pear!")
+    totalNumPears++;
+}else{
+    //This code will run if the if statement is FALSE!
+    console.log("This fruit is not a pear!")
+}
