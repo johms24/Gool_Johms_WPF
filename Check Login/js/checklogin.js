4592ee9a539fd4431598608feafcbb884f152cd3
johms24 <rjg@iname.com>
2015-01-22 23:49:52 -0500

    Completed Check Login JS
    Completed Check Login JS

---
+++ Check Login/js/checklogin.js
@@ -0,0 +1,28 @@
+/**
+ * Created by rjgi on 1/22/2015.
+ */
+
+//alert("test");
+
+//assign variables for correct username and password
+var correctU="admin";
+var correctP="fullsail1";
+
+//prompt user for username; and prompt for password, if username is correct
+var usernameInput=prompt("Username?");
+
+if(usernameInput==correctU){
+    var passwordInput=prompt("Password?");
+    if(passwordInput==correctP){
+        alert("Welcome " +usernameInput+ "!");
+        console.log("Welcome " +usernameInput+ "!");
+    }else{
+        alert("Incorrect password. Try again.");
+        console.log("Incorrect password. Try again.");
+    };
+}else{
+    alert("Unknown user.  Try again.");
+    console.log("Unknown user.  Try again.");
+};
+
+
