db55f898392a6c5871c6a10c946232fb06ee3cf6
johms24 <rjg@iname.com>
2015-01-24 16:46:30 -0500

    Changed the wording on the printout
    Changed the wording on the printout

--- Check Login/js/checklogin.js
+++ Check Login/js/checklogin.js
@@ -14,15 +14,15 @@ var usernameInput=prompt("Username?");
 if(usernameInput==correctU){
     var passwordInput=prompt("Password?");
     if(passwordInput==correctP){
-        alert("Welcome " +usernameInput+ "!");
-        console.log("Welcome " +usernameInput+ "!");
+        alert("Welcome, " +usernameInput+ "!");
+        console.log("Welcome, " +usernameInput+ "!");
     }else{
-        alert("Incorrect password. Try again.");
-        console.log("Incorrect password. Try again.");
+        alert("Password does not match our records.");
+        console.log("Password does not match our records.");
     };
 }else{
-    alert("Unknown user.  Try again.");
-    console.log("Unknown user.  Try again.");
+    alert("User not found. Try again.");
+    console.log("User not found. Try again.");
 };
 
 
