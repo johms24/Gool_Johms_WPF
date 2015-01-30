7e11a978b019fe630d8b2d8f3ddc4d04bfe86071
johms24 <rjg@iname.com>
2015-01-30 17:47:52 -0500

    Messed around with the randomizer
    Put my personal touch on it!!!

--- Week 4/Randomizer/js/randomizer.js
+++ Week 4/Randomizer/js/randomizer.js
@@ -7,13 +7,44 @@ alert("We are going to get random numbers between two numbers of your choice.")
 var min=prompt("Min number?");
 var max=prompt("Max number?");
 
+//Check entry for proper data
+while(isNaN(min) || isNaN(max)) {
+    min = prompt("Numbers only, Genius!!!  Try again, but look up what a number is, " +
+    "first.  Then, enter numbers!!!  Let's start with the minimum number in the box " +
+    "right here. Thanks.");
+    max = prompt("Now, the max NUMBER, please!!!");
+}
+
+while(Number(min)>=Number(max) || min=="" || max==""){
+    min = prompt("Look, Mr. or Ms. BRILLIANT!  The minimum number has to be LESS " +
+    "THAN the maximum number, and you have to put SOMETHING in there, okay???  " +
+    "Let's try it again.  Minimum number first, in the box! No blanks!!!");
+    max = prompt("Now for the maximum number.  Remember it has to be ''bigger'' than " +
+    "the one you just entered.");
+}
 
 function randomizer(minNum, maxNum){
 
     //Math.random() * (max number - min number) + min number
     var randomNumber = Math.round(Math.random() * (maxNum - minNum) + Number(minNum));
-    console.log(randomNumber);
+    //No longer needed within the function: console.log(randomNumber);
+    //Return value to main code.
     return Number(randomNumber);
 }
 
-console.log(randomizer(min, max));
+var result=randomizer(min, max);
+console.log(result);
+
+
+console.log("Starting loop.")
+//Give me 5 random numbers.
+
+//Use a loop - for loop -
+//for(declare a variable; condition to test; increment of change){code}
+
+for(var i=0; i<7; i++){
+
+    console.log(randomizer(min, max));
+    alert(randomizer(min,max));
+
+}
